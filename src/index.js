ebfcb0def18bdc662b874aad9ef247c72b74ffb1
Rebecca Kibisu <coursecpd@gmail.com>
2025-07-10 18:40:44 +0300

    codechallenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent page reload
+
+    const taskText = input.value;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+    form.reset();
+  });
 });
